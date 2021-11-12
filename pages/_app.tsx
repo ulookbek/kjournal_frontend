4c1f4f7d5dbde804a9f722646be4973393ce415b
Улукбек Капарбеков <ulukbekkaparbekov@gmail.com>
2021-11-12 14:06:58 +0600

    Upgrade mui version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css';
-import 'macro-css';
+import Head from "next/head";
+import {CssBaseline} from "@mui/material";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Head>
+          <title>KJournal</title>
+          <link rel="icon" href="/favicon.ico" />
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+          <CssBaseline />
+        <Component {...pageProps} />
+      </>
+  )
 }
 
 export default MyApp
